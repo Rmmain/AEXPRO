425ff225e34a7d8f85a2c328bbef7edc158b8dae
Leray <romain.leray@icloud.com>
2024-10-09 15:42:27 +0200

    Initial Commit

---
+++ AEXPRO/AEXPROApp.swift
@@ -0,0 +1,17 @@
+//
+//  AEXPROApp.swift
+//  AEXPRO
+//
+//  Created by Romain LERAY on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AEXPROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AEXPROTests/AEXPROTests.swift
@@ -0,0 +1,16 @@
+//
+//  AEXPROTests.swift
+//  AEXPROTests
+//
+//  Created by Romain LERAY on 09/10/2024.
+//
+
+import Testing
+
+struct AEXPROTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
